93c61b60b9904c1a288662a2651c48f724c717a0
Jeff Foster <jeff.foster@acm.org>
2014-09-17 18:39:26 +0100

    include csv parser, make module clearer

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
+module Main where
 
 import Diagrams.Prelude hiding (duration)
 import Diagrams.Backend.SVG.CmdLine
@@ -9,27 +10,25 @@ import Project
 verticalSpacing :: Double
 verticalSpacing = 5
 
-
-
 -- Eventually, I'd parse this from a file
 projects :: [Project]
 projects = [
-    Project "Develop UI"              Improvement 400   100
-  , Project "Research shoe size"      Learn       100    20
-  , Project "Make financial plans"    Sustain     30     10
-  , Project "Add formatting features" Execute     30     80
+    Project "Develop UI"              Improvement 4000   1000
+  , Project "Research shoe size"      Learn       1000    200
+  , Project "Make financial plans"    Sustain     300     100
+  , Project "Add formatting features" Execute     300     800
   ]
 
 main :: IO ()
-main = mainWith (vcat $ map (alignL . visualize) projects)
+main = mainWith $ vcat $ map (alignL . visualize) projects
 
 visualize :: Project -> Diagram B R2
-visualize p = text t <> (rect w h # fc c === strutY verticalSpacing)
+visualize p = (text d) <> ((rect w h # fc c) === strutY verticalSpacing)
   where
     w = fromIntegral $ cost p
     h = fromIntegral $ duration p
     c = colorFrom $ outcome p
-    t = name p
+    d = name p
 
 colorFrom :: (Ord a, Floating a) => Outcome -> Colour a
 colorFrom Improvement = blue
@@ -37,8 +36,3 @@ colorFrom Learn       = red
 colorFrom Sustain     = yellow
 colorFrom Execute     = green
 
-example :: Diagram B R2
-example = circle 1 # fc blue
-                   # lw veryThick
-                   # lc purple
-                   # dashingG [0.2,0.05] 0
