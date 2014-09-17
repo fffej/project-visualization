2adea2e23b2b6ae759839c4c3e559bcd9b061c9a
Jeff Foster <jeff.foster@acm.org>
2014-09-17 19:15:04 +0100

    add appropriate instances so I can parse things

--- src/Project.hs
+++ src/Project.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Project
        (
          Outcome(..),
@@ -5,11 +7,17 @@ module Project
        )
        where
 
+import Data.Csv
+import Data.Vector
+import Data.ByteString
+import Control.Applicative ((<$>), (<*>), pure)
+import Control.Monad (mzero)
+
 data Outcome = Execute
              | Improvement
              | Learn
              | Sustain
-               deriving (Show,Eq)
+               deriving (Read,Show,Eq)
 
 data Project = Project
                {
@@ -19,3 +27,17 @@ data Project = Project
                , duration :: Int
                } deriving (Show)
                  
+instance FromNamedRecord Project where
+  parseNamedRecord m = Project <$>
+                       m .: "Name" <*>
+                       m .: "Outcome" <*>
+                       m .: "Cost" <*>
+                       m .: "Duration"
+
+instance FromField Outcome where
+  parseField s
+    | s == "Execute" = pure Execute
+    | s == "Improvement" = pure Improvement
+    | s == "Learn" = pure Learn
+    | s == "Sustain" = pure Sustain
+    | otherwise = mzero
